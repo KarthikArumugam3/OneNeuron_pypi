0a46a03623c30c0913f94a0845473285e800a2a7
Karthik Arumugam <69972911+KarthikArumugam3@users.noreply.github.com>
2021-09-26 13:35:30 +0530

    updated readme

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "KarthikArumugam3"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.1",
+    version="0.0.2",
     author= USER_NAME,
     author_email="karthik131100@gmail.com",
     description="It's an implementation of perceptron",
@@ -28,6 +28,6 @@ setuptools.setup(
     python_requires=">=3.7",
     install_requires=[
         "numpy",
-        "tqdm"          
+        "tqdm"           
     ]
 )
